e6ef2e96c509cf34cf199c4771d0ae40f979b2ec
Sergey Yashchenko <yashenko.s@gmail.com>
2014-12-15 01:18:56 +0200

    Dependency injection container added.

--- app/Console/Application.php
+++ app/Console/Application.php
@@ -1,26 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sergey
- * Date: 13.12.14
- * Time: 20:51
- */
 
 namespace Application\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Application extends BaseApplication
+class Application extends BaseApplication implements ContainerAwareInterface
 {
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
     protected function getDefaultCommands()
     {
         return array_merge(
             parent::getDefaultCommands(),
             array(
-                new Command\SetScoreCommand(),
+                new Command\ListScoresCommand(),
                 new Command\GetScoreCommand(),
+                new Command\SetScoreCommand(),
                 new Command\IncrementScoreCommand(),
                 new Command\DecrementScoreCommand(),
+                new Command\RemovePlayerCommand(),
             )
         );
     }
